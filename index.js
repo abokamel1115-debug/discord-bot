0941b2f33ac33239daf7912091f6b549fda1462a
abokamel1115-debug <abokamel1115@gmail.com>
2026-04-03 14:43:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { startMessages } = require('./messages');
+const { handleXP, getLevel } = require('./levels');
+const express = require('express');
+const { connectDB } = require('./database'); // 👈 مهم
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bot is');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🌐 Server running on port ${PORT}`);
+});
+
+// 🔥 الاتصال بقاعدة البيانات
+connectDB();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+const TOKEN = process.env.TOKEN;
+
+client.once('ready', () => {
+    console.log(`🔥 Logged in as ${client.user.tag}`);
+    startMessages(client);
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+
+    await handleXP(message);
+
+    if (message.content === "!ping") {
+        message.reply("🏓 Pong from Home!");
+    }
+
+    if (message.content === "!level") {
+        await getLevel(message);
+    }
+});
+
+process.on('unhandledRejection', err => console.error(err));
+process.on('uncaughtException', err => console.error(err));
+
+client.login(TOKEN);
---
+++ levels.js
@@ -0,0 +1,52 @@
+const { getDB } = require('./database'); // 👈 بدل index
+
+async function handleXP(message) {
+    const db = getDB();
+    if (!db) return;
+
+    const users = db.collection("users");
+    const userId = message.author.id;
+
+    let user = await users.findOne({ userId });
+
+    if (!user) {
+        user = { userId, xp: 0, level: 1 };
+        await users.insertOne(user);
+    }
+
+    user.xp += 3;
+
+    const neededXP = user.level * 100;
+
+    if (user.xp >= neededXP) {
+        user.level++;
+        user.xp = 0;
+
+        message.channel.send(`🔥 ${message.author} وصل Level ${user.level} 😈`);
+    }
+
+    await users.updateOne(
+        { userId },
+        { $set: { xp: user.xp, level: user.level } }
+    );
+}
+
+async function getLevel(message) {
+    const db = getDB();
+    if (!db) return;
+
+    const users = db.collection("users");
+    const userId = message.author.id;
+
+    const user = await users.findOne({ userId });
+
+    if (!user) {
+        return message.reply("😈 أنت لسه Level 0... ابدأ اكتب!");
+    }
+
+    message.reply(
+        `🔥 Level: ${user.level}\n💀 XP: ${user.xp}`
+    );
+}
+
+module.exports = { handleXP, getLevel };
